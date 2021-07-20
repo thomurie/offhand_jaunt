1d00ae139061fb6517c58f1a57832d9216d52255
Thomas Urie <uriethom@gmail.com>
2021-07-19 19:35:37 -0600

    increased security

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 
 from flask import Flask, render_template, redirect, session, jsonify, g, request
 from models import connect_db, db
-from validation import random_choice, data, rapid_api_key, unsplash_key
+from validation import random_choice, data, rapid_api_key, unsplash_key, flask_key
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = flask_key
 
 connect_db(app)
 
--- validation.py
+++ validation.py
@@ -1,7 +1,7 @@
 import random
 filename = f'iata_codes.txt'
-with open(filename, 'r') as f:
-        data = f.read().split()
+with open(filename, 'r') as a:
+        data = a.read().split()
 
 def random_choice(arr):
     random_index = random.randint(0,len(arr)-1)
@@ -11,12 +11,19 @@ def random_choice(arr):
 rapid_api_key = None
 
 rapid_api = f'keys/rapid_api.txt'
-with open(rapid_api, 'r') as f:
-    rapid_api_key = f.read()
+with open(rapid_api, 'r') as b:
+    rapid_api_key = b.read()
 
 unsplash_key = None
 
 unsplash_api = f'keys/unsplash.txt'
 
-with open(unsplash_api, 'r') as g:
-    unsplash_key = g.read()
+with open(unsplash_api, 'r') as c:
+    unsplash_key = c.read()
+
+flask_key = None
+
+flask_file = f'keys/flask_key.txt'
+
+with open(flask_file, 'r') as d:
+    flask_key = d.read()
